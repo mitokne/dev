ffb12c326cab687873314696b49f8c3923a32fd1
mitokne <hisui05mk@gmail.com>
2018-08-31 17:50:47 +0900

    コメントのセミコロン削除

---
+++ Task1.php
@@ -0,0 +1,21 @@
+<?php
+
+$map = array(1,2,3,4,5);
+
+var_dump($map);
+
+//配列のvalueにカンマを追加して出力
+foreach ($map as $value) {
+  // code...
+  $str = $value . ",";
+
+ echo $str;
+
+// var_dump($str)
+
+}
+
+
+// var_dump($task1list);
+
+ ?>
